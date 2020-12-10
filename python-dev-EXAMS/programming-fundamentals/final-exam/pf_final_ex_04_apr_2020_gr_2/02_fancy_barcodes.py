7183629aac81050617b5b9eebb830dc04a00fb8c
a-kbv <atanaskorabov@gmail.com>
2020-12-10 14:31:05 +0200

    commit

---
+++ python-dev-EXAMS/programming-fundamentals/final-exam/pf_final_ex_04_apr_2020_gr_2/02_fancy_barcodes.py
@@ -0,0 +1,14 @@
+import re
+
+pattern = '@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+'
+n = int(input())
+for _ in range(n):
+    barcode_line = input()
+    if re.match(pattern, barcode_line):
+        digits = re.findall(r'\d', barcode_line)
+        if digits:
+            print(f"Product group: {''.join(digits)}")
+        else:
+            print("Product group: 00")
+    else:
+        print("Invalid barcode")
