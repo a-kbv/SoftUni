878655b614016ee1e0cf7067ab458df1d71ee0e0
a-kbv <atanaskorabov@gmail.com>
2020-12-12 16:58:19 +0200

    commit

--- python-dev-advanced/tuples_and_sets_lab/01_count_same_values.py
+++ python-dev-advanced/tuples_and_sets_lab/01_count_same_values.py
@@ -1,6 +1,17 @@
-def occurences():
-    tupl = (int(el) for el in input().split())
-    
 
+def count_numbers(tupl, t_dict):
+    for el in tupl:
+        if el not in t_dict:
+            t_dict[el] = 1
+        else:
+            t_dict[el] += 1
+    return t_dict
+
+def occurences():
+    tupl = (float(el) for el in input().split())
+    t_dict = {}
+    count_numbers(tupl, t_dict)
+    for key, value in t_dict.items():
+        print(f"{key} - {value} times")
 
 occurences()
