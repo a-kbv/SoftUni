92718eef519d3a7540bb1ba03e0994a2384e898c
Atanas Korabov <67587336+a-kbv@users.noreply.github.com>
2020-12-18 17:08:10 +0200

    Update 03_fitness_card.py

--- python-dev-EXAMS/programming-basic/PB_exam_28-29_mar_2020/03_fitness_card.py
+++ python-dev-EXAMS/programming-basic/PB_exam_28-29_mar_2020/03_fitness_card.py
@@ -1,70 +1,24 @@
-#   FITENSS CARD
+#   FITNESS CARD
 
-sport = ['Gym', 'Boxing', 'Yoga', 'Zumba', 'Dances', 'Pilates']
-man = [42, 41, 45, 34, 51, 39]
-woman = [35, 37, 42, 31, 53, 37]
-
-budget = float(input())
-sex = input()
-y_o = int(input())
-sport_i = input()
+sport = ('Gym', 'Boxing', 'Yoga', 'Zumba', 'Dances', 'Pilates')
+man = (42, 41, 45, 34, 51, 39)
+woman = (35, 37, 42, 31, 53, 37)
+budget, sex, y_o, sport_i = float(input()), input(), int(input()), input()
 
 sum = 0
 
-if sex == "m": # man
-    if y_o > 19:
-        if sport_i == sport[0]:
-            sum = man[0]
-        if sport_i == sport[1]:
-            sum = man[1]
-        if sport_i == sport[2]:
-            sum = man[2]
-        if sport_i == sport[3]:
-            sum = man[3]
-        if sport_i == sport[4]:
-            sum = man[4]
-        if sport_i == sport[5]:
-            sum = man[5]
-    else:
-        if sport_i == sport[0]:
-            sum = man[0] * 0.8
-        if sport_i == sport[1]:
-            sum = man[1] * 0.8
-        if sport_i == sport[2]:
-            sum = man[2] * 0.8
-        if sport_i == sport[3]:
-            sum = man[3] * 0.8
-        if sport_i == sport[4]:
-            sum = man[4] * 0.8
-        if sport_i == sport[5]:
-            sum = man[5] * 0.8
-elif sex == "f": # woman
-    if y_o > 19:
-        if sport_i == sport[0]:
-            sum = woman[0]
-        if sport_i == sport[1]:
-            sum = woman[1]
-        if sport_i == sport[2]:
-            sum = woman[2]
-        if sport_i == sport[3]:
-            sum = woman[3]
-        if sport_i == sport[4]:
-            sum = woman[4]
-        if sport_i == sport[5]:
-            sum = woman[5]
-    else:
-        if sport_i == sport[0]:
-            sum = woman[0] * 0.8
-        if sport_i == sport[1]:
-            sum = woman[1] * 0.8
-        if sport_i == sport[2]:
-            sum = woman[2] * 0.8
-        if sport_i == sport[3]:
-            sum = woman[3] * 0.8
-        if sport_i == sport[4]:
-            sum = woman[4] * 0.8
-        if sport_i == sport[5]:
-            sum = woman[5] * 0.8
+for i in range(len(sport)):
+    if sport_i == sport[i]:
+        if y_o > 19:
+            if sex == "m":
+                sum = man[i]
+            elif sex == "f":
+                sum = woman[i]
+        else:
+            if sex == "m":
+                sum = man[i] * 0.8
+            elif sex == "f":
+                sum = woman[i] * 0.8
 
 if budget >= sum:
     print(f"You purchased a 1 month pass for {sport_i}.")
