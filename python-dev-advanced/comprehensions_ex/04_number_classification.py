d867ac406a843b62bd237e1a24b77bb06dae3254
a-kbv <atanaskorabov@gmail.com>
2020-12-18 23:09:55 +0200

    commit

--- python-dev-advanced/comprehensions_ex/04_number_classification.py
+++ python-dev-advanced/comprehensions_ex/04_number_classification.py
@@ -1,2 +1,6 @@
-line = int(input())
+line = [int(el) for el in input().split(', ')]
 
+print(f"Positive: {', '.join([str(el) for el in line if el >= 0])}")
+print(f"Negative: {', '.join([str(el) for el in line if el < 0])}")
+print(f"Even: {', '.join([str(el) for el in line if el % 2 == 0])}")
+print(f"Odd: {', '.join([str(el) for el in line if el % 2 == 1])}")
