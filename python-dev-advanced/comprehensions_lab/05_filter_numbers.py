b7f893301fd30dedde11320bc0692b75793b528c
a-kbv <atanaskorabov@gmail.com>
2020-12-18 22:25:43 +0200

    commit

---
+++ python-dev-advanced/comprehensions_lab/05_filter_numbers.py
@@ -0,0 +1,8 @@
+start, end = int(input()), int(input())
+
+def is_num_valid(number):
+    return len([True for d in range(2,11) if number % d == 0]) > 0
+
+print(
+    [x for x in range(start, end + 1) if is_num_valid(x)]
+)
