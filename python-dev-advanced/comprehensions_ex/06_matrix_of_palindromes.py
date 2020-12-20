8985e37026ee42dca05324c7cc233be226e99311
a-kbv <atanaskorabov@gmail.com>
2020-12-20 23:47:48 +0200

    commit

--- python-dev-advanced/comprehensions_ex/06_matrix_of_palindromes.py
+++ python-dev-advanced/comprehensions_ex/06_matrix_of_palindromes.py
@@ -2,20 +2,16 @@
 def read_int_list_from_input(separator=' '):
     return [int(x) for x in input().split(separator)]
 
-
-# a = 97
-# b = 98
-# c = 99
 def palindromic_matrix(r,c):
-    matrix = [[]*r for _ in range(r)]
 
-    for row in range(r):
-        for col in range(c):
-            matrix[row].append(f"{chr(97+row)}"
-                          f"{chr(97 + row + col)}"
-                          f"{chr(97+row)}")
+    # matrix = [[]*r for _ in range(r)]
+    # for row in range(r):
+    #     for col in range(c):
+    #         matrix[row].append(f"{chr(97+row)}"
+    #                       f"{chr(97 + row + col)}"
+    #                       f"{chr(97+row)}")
 
-    return matrix
+    return [[f"{chr(row)}{chr(row + col)}{chr(row)}" for col in range(c)]for row in range(97, 97+r)]
 
 
 if __name__ == "__main__":
