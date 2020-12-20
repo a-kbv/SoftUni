7151df94b15dff2c2ea4ade549f62258be650551
a-kbv <atanaskorabov@gmail.com>
2020-12-21 00:15:30 +0200

    commit

--- python-dev-advanced/comprehensions_ex/07_flatten_list.py
+++ python-dev-advanced/comprehensions_ex/07_flatten_list.py
@@ -1,4 +1,4 @@
 # ex input : 1 2 3 |4 5 6 |  7  8
 
-print(' '.join(el for x in input().split('|')[::-1] for el in x.split(" ") if el != ""))
+print(' '.join(el for x in input().split('|')[::-1] for el in x.split()))
 
