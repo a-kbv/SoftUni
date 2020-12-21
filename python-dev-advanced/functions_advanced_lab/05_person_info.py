823de990cf5defc9e37c9a4489283db157a6a44e
a-kbv <atanaskorabov@gmail.com>
2020-12-21 16:55:08 +0200

    commit

--- python-dev-advanced/functions_advanced_lab/05_person_info.py
+++ python-dev-advanced/functions_advanced_lab/05_person_info.py
@@ -1,3 +1,5 @@
 def get_info(**kwargs):
     return f"This is {kwargs['name']} from {kwargs['town']} " \
            f"and he is {kwargs['age']} years old"
+
+print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
