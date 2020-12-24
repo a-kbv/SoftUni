19134a716ed91c8155cd11b8ba61709b066f7f80
a-kbv <atanaskorabov@gmail.com>
2020-12-24 17:36:35 +0200

    commit

--- python-dev-advanced/functions_advanced_ex/01_even _numbers.py
+++ python-dev-advanced/functions_advanced_ex/01_even _numbers.py
@@ -1,4 +1,4 @@
 def even_nums(*nums):
     return [el for el in nums if el % 2 == 0]
 
-even_nums([int(el) for el in input().split()])
+print(even_nums(*[int(el) for el in input().split()]))
---
+++ python-dev-advanced/functions_advanced_ex/02_sort.py
@@ -0,0 +1,4 @@
+def sort_nums_to_list(*nums):
+    return sorted([int(el) for el in input().split()])
+
+print(sort_nums_to_list())
