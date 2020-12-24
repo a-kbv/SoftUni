d24973c89dedc0d421f21af8d8661d3994fd4bc8
a-kbv <atanaskorabov@gmail.com>
2020-12-24 18:22:04 +0200

    commit

---
+++ python-dev-advanced/functions_advanced_ex/08_even_odd.py
@@ -0,0 +1,10 @@
+def even_odd(*nums):
+    nums = list(nums)
+    if nums[-1] == "even":
+        nums.pop()
+        return [num for num in nums if num % 2 == 0]
+    nums.pop()
+    return [num for num in nums if num % 2 == 1]
+
+
+print(even_odd(1, 2, 3, 4, 5, 6, "even"))
