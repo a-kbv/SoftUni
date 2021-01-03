cbf0d709f2f5edaffa205d65bbeeb8f9076e3e4f
a-kbv <atanaskorabov@gmail.com>
2021-01-03 18:41:04 +0200

    commit

--- python-dev-advanced/error_handling_ex/02_email_validator.py
+++ python-dev-advanced/error_handling_ex/02_email_validator.py
@@ -1,50 +1,46 @@
 
-class Error(Exception):
-    """Base class Exception"""
-    pass
-class NameTooShortError(Error):
+
+class NameTooShortError(Exception):
     """Raised when name is less than or equal to 4 symbols"""
-    pass
-class MustContainAtSymbolError(Error):
+    def __init__(self, message="Name must be more than 4 characters"):
+        self.message = message
+        super().__init__(message)
+
+class MustContainAtSymbolError(Exception):
     """Raise when there is no "@" in the email"""
-    pass
-class InvalidDomainError(Error):
+    def __init__(self, message="Email must contain @"):
+        self.message = message
+        super().__init__(message)
+
+class InvalidDomainError(Exception):
     """Raise when the domain of the email is invalid ( valid are: .com, .bg, .org, .net)"""
-    pass
+    def __init__(self, message="Domain must be one of the following: .com, .bg, .org, .net"):
+        self.message = message
+        super().__init__(message)
 
-def get_name(email):
-    name = []
-    for el in email:
-        if el != '@':
-            name.append(el)
-        else:
-            break
-    return ''.join(name)
+def validate_name(email):
+    username = email.split("@")[0]
+    if len(username) <= 4:
+        raise NameTooShortError()
 
+def validate_at_symbol(email):
+    if "@" not in email:
+        raise MustContainAtSymbolError()
+
+def validate_domain(email, valid_domains):
+    domain = email.split(".")[-1]
+    if domain not in valid_domains:
+        raise InvalidDomainError()
 
 while True:
-    try:
-        email = input()
-        name = get_name(email)
+    line = input()
+    valid_domains = ("com", "net", "bg", "org")
 
-        if len(name) <=4:
-            raise NameTooShortError
+    if line == "End":
+        break
 
-        if "@" not in email:
-            raise MustContainAtSymbolError
+    validate_name(line)
+    validate_at_symbol(line)
+    validate_domain(line, valid_domains)
 
-        if ".com" not in email or\
-            ".bg" not in email or\
-            ".org" not in email or\
-            ".net" not in email:
-            raise InvalidDomainError
-
-
-    except NameTooShortError:
-        print("Name must be more than 4 characters")
-
-    except MustContainAtSymbolError:
-        print("Email must contain @")
-
-    except InvalidDomainError:
-        print("Domain must be one of the following: .com, .bg, .org, .net")
+    print("Email is valid")
